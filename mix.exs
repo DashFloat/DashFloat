3c4fd689e76beaf8fe13d327f7c16228a06b9f13
Terence Ponce <terence@dashfloat.com>
2024-04-13 07:53:18 +0100

    Integrate Dialyzer (#7)

--- mix.exs
+++ mix.exs
@@ -17,6 +17,13 @@ defmodule DashFloat.MixProject do
         "coveralls.post": :test,
         "coveralls.html": :test,
         "coveralls.cobertura": :test
+      ],
+      dialyzer: [
+        plt_add_apps: [
+          :ex_unit
+        ],
+        plt_file: {:no_warn, "priv/plts/project.plt"},
+        list_unused_filter: true
       ]
     ]
   end
@@ -42,6 +49,7 @@ defmodule DashFloat.MixProject do
     [
       {:bandit, "1.4.2"},
       {:credo, "1.7.5", only: [:dev, :test], runtime: false},
+      {:dialyxir, "1.4.3", only: [:dev, :test], runtime: false},
       {:dns_cluster, "0.1.3"},
       {:ecto_sql, "3.11.1"},
       {:esbuild, "0.8.1", runtime: Mix.env() == :dev},
